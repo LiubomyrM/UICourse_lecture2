0fcf78dcbfb0e7ca626be1e092676c886f4f09d6
LiubomyrM <mrlubylu@gmail.com>
2015-05-05 19:54:18 +0300

    lecture 3 changes

---
+++ app.js
@@ -0,0 +1,13 @@
+/*function submitJS(){
+    var vTitle = document.getElementById("atitle").value;
+    var vBody = document.getElementById("abody").value;
+    
+    var newH = document.createElement("h4");
+    var newPara = document.createElement("p");
+    newH.appendChild(vTitle)
+    newPara.appendChild(vBody);
+    
+    var getATitle = document.getElementById("atitle")
+    var getArticle = document.getElementById("atext")
+    document.body.insertBefore
+}*/
